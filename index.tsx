c074d5c2e9560184f9261a37b7d2300ff1b8722c
zianad <infinitydecosign@gmail.com>
2025-10-20 14:44:57 +0100

    Adding the new feature

--- index.tsx
+++ index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
